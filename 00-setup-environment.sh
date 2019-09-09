56054d5d0111cd27a68a062cf9da0d231c65fa81
Markus Roth <contact-dd-pose-dataset@tudelft.nl>
2019-09-10 01:51:18 +0200

    Robustify install script.

--- 00-setup-environment.sh
+++ 00-setup-environment.sh
@@ -15,13 +15,17 @@ if [ ! -f $DD_POSE_DIR/venv/bin/activate ]; then
     virtualenv $DD_POSE_DIR/venv
 fi
 source $DD_POSE_DIR/venv/bin/activate
-pip install -r $DD_POSE_DIR/requirements.txt
+pip install pip --upgrade
+pip install setuptools --upgrade # to fix pip install behind proxy
+pip install numpy --upgrade # to fix transformations install
+pip install -r $DD_POSE_DIR/requirements.txt --upgrade
 
 
 # orca for plotly export
 # https://github.com/plotly/orca/releases
 # https://github.com/plotly/orca#linux
+echo "Downloading orca binary..."
 mkdir -p $DD_POSE_DIR/bin
 curl -L https://github.com/plotly/orca/releases/download/v1.2.1/orca-1.2.1-x86_64.AppImage -o $DD_POSE_DIR/bin/orca-1.2.1-x86_64.AppImage
-ln -s orca-1.2.1-x86_64.AppImage $DD_POSE_DIR/bin/orca
+ln -f -s orca-1.2.1-x86_64.AppImage $DD_POSE_DIR/bin/orca
 chmod +x $DD_POSE_DIR/bin/orca
