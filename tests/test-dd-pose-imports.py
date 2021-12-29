2aa1e93636f8927f182131a90bafad790597d5ec
Markus Roth <contact-dd-pose-dataset@tudelft.nl>
2021-12-29 15:27:09 +0000

    Add test for imports.

---
+++ tests/test-dd-pose-imports.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import dd_pose.dataset
+import dd_pose.dataset_item
+import dd_pose.evaluation_helpers
+import dd_pose.image_decorator
+import dd_pose.jupyter_helpers
+import dd_pose.pinhole_camera_model
+import dd_pose.pinhole_camera_model_serializer
+import dd_pose.visualization_helpers
